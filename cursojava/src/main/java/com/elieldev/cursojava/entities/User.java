03129b406ba926f28017aaca2739d3249d3c7297
elielm14 <elieljuniorm@gmail.com>
2023-03-16 15:49:30 -0300

    criando estrutura do banco h2
    configurando e testando com sucesso o banco de dados h2 em tempo de desenvolvimento

--- cursojava/src/main/java/com/elieldev/cursojava/entities/User.java
+++ cursojava/src/main/java/com/elieldev/cursojava/entities/User.java
@@ -1,12 +1,22 @@
 package com.elieldev.cursojava.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_user") //modifica o nome da tabela no banco para nao ter conflito, User é palavra reservada no banco
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L; //numero de série padrão(opcional)
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) //gera o numero automatico de id no banco
   private Long id;
+
   private String name;
   private String email;
   private String phone;
